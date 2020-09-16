82d4a62e5a0768327043c5fb72ca288408d4493d
JPSS14 <jp_scarabelli@hotmail.com>
2020-09-15 22:57:31 -0300

    Clone IGN Brasil 1.0

---
+++ IGN/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Styles/boot.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Sony fechará fábrica em Manaus em 2021</title>
+</head>
+<body>
+    <h1>IGN</h1>
+</body>
+</html>
