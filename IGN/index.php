87a583adeceb88b7c352a582f9a05e63e10b2e80
JPSS14 <jp_scarabelli@hotmail.com>
2020-09-17 23:33:53 -0300

    Clone IGN Brasil UPDATE 1: Added header

--- IGN/index.php
+++ IGN/index.php
@@ -1,13 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Styles/boot.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Themes/IGN/style.css">
     <title>Sony fechará fábrica em Manaus em 2021</title>
 </head>
+
 <body>
-    <h1>IGN</h1>
+    <header class="main_header">
+        <div class="main_header_container">
+            <div class="nav_logo">
+                <div class="logo">
+                    <img src="Uploads/ign-logo.png" alt="IGN Logo" title="IGN Logo" />
+                </div>
+            </div>
+            <div class="nav">
+                <ul class="nav_menu">
+                    <li><a href="#">PS4</a></li>
+                    <li><a href="#">XOne</a></li>
+                    <li><a href="#">Switch</a></li>
+                    <li><a href="#">PC</a></li>
+                    <li><a href="#">Acer</a></li>
+                    <li><a href="#">Ver Mais</a></li>
+                    <li><a href="#">Search</a></li>
+                    <li class="btn"><a href="#">Entrar</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="subnav">
+            <ul class="subnav_menu">
+                <li><a href="#">Dicas de Fall Guys</a></li>
+                <li><a href="#">Quiz de Attack on Titan: Quem você seria?</a></li>
+                <li><a href="#">Os melhores jogos de anime</a></li>
+                <li><a href="#">The Last of Us 2: Cofres e códigos</a></li>
+            </ul>
+        </div>
+
+    </header>
 </body>
+
 </html>
